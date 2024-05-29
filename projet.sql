e611680ba26bf983b31674d8575efa17133687fa
pacoville <115997842+pacoville@users.noreply.github.com>
2024-05-29 11:21:10 +0200

    Create projet.sql

---
+++ projet.sql
@@ -0,0 +1,28 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : mer. 29 mai 2024 à 09:19
+-- Version du serveur : 5.7.39
+-- Version de PHP : 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `Projet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Administrateur`
+--
