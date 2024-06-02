7f414ef99553ccb395b219e04171059d47e6c168
rayan213dz <129366342+rayan213dz@users.noreply.github.com>
2024-06-02 16:57:01 +0200

    Create rechercher.php

---
+++ rechercher.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Variables de connexion à la base de données
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "piscine";
+
+    // Connexion à la base de données
+    try {
+        $dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // Configuration du mode d'erreur PDO pour afficher les erreurs
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die('Erreur de connexion à la base de données : ' . $e->getMessage());
+    }
+
+    // Récupération du terme de recherche du formulaire
+    $search_term = $_POST["search_term"] ?? '';
+
+   
