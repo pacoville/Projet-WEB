0576993077c929033076081dbadbc210fecd2f23
rayan213dz <129366342+rayan213dz@users.noreply.github.com>
2024-06-02 16:57:50 +0200

    Update rechercher.php

--- rechercher.php
+++ rechercher.php
@@ -19,5 +19,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Récupération du terme de recherche du formulaire
     $search_term = $_POST["search_term"] ?? '';
+     // Préparation et exécution de la requête SQL pour rechercher le coach par nom ou spécialité
+    $requete = $dbh->prepare("SELECT * FROM Coach 
+                              INNER JOIN Utilisateur ON Coach.utilisateur_id = Utilisateur.utilisateur_id 
+                              WHERE Utilisateur.prenom = ? OR Coach.specialite = ?");
+    $requete->bindParam(1, $search_term, PDO::PARAM_STR);
+    $requete->bindParam(2, $search_term, PDO::PARAM_STR);
+    $requete->execute();
 
-   
+    // Récupération des résultats de la recherche
+    $coaches = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+    // Vérification s'il y a des résultats de recherche pour les coachs
+    if ($coaches) {
+        // Affichage des résultats de la recherche pour les coachs
+        echo "<h2>Résultats de la recherche pour '" . htmlspecialchars($search_term) . "' :</h2>";
+        foreach ($coaches as $coach) {
+            echo "<p>Nom : " . htmlspecialchars($coach['nom']) . "</p>";
+            echo "<p>Prénom : " . htmlspecialchars($coach['prenom']) . "</p>";
+            echo "<p>Spécialité : " . htmlspecialchars($coach['specialite']) . "</p>";
+            echo "<hr>";
+        }
+    } else {
+        // Aucun résultat trouvé pour les coachs
+        echo "<h2>Aucun coach trouvé pour la recherche'" . htmlspecialchars($search_term) . "'</h2>";
+    }
+
+    
