925beb5ced00f84a528fa35b84527718aa6fd279
rayan213dz <129366342+rayan213dz@users.noreply.github.com>
2024-06-02 17:06:49 +0200

    Update rechercher_etablissement.php

--- rechercher_etablissement.php
+++ rechercher_etablissement.php
@@ -19,7 +19,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     // Récupération du terme de recherche du formulaire
     $search_etablissement = trim($_POST["search_etablissement"]);
-
-    
+    // Préparation et exécution de la requête SQL pour rechercher l'établissement par nom (insensible à la casse)
+    $requete = $dbh->prepare("SELECT * FROM établissement WHERE LOWER(nom) = LOWER(?)");
+    $requete->bindParam(1, $search_etablissement, PDO::PARAM_STR);
+    $requete->execute();
 
 
